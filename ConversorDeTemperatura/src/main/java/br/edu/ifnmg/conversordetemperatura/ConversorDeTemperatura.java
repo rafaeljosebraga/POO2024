4b73257361865748cbb72d964e3a213b434dfa15
Rafael José <braga3.coelho3@gmail.com>
2024-07-03 20:02:54 -0300

    Colocando os arquivos gerados pelo netbeans.

---
+++ ConversorDeTemperatura/src/main/java/br/edu/ifnmg/conversordetemperatura/ConversorDeTemperatura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifnmg.conversordetemperatura;
+
+/**
+ *
+ * @author braga
+ */
+public class ConversorDeTemperatura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
