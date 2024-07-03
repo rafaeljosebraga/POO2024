cbfa5b6398713e20539ac34c2d39070776761b34
Rafael José <braga3.coelho3@gmail.com>
2024-07-03 20:39:37 -0300

    Variáveis da classe definidas.

--- ConversorDeTemperatura/src/main/java/br/edu/ifnmg/conversordetemperatura/Temperatura.java
+++ ConversorDeTemperatura/src/main/java/br/edu/ifnmg/conversordetemperatura/Temperatura.java
@@ -1,15 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package br.edu.ifnmg.conversordetemperatura;
 
 /**
  *
  * @author braga
  */
-public class ConversorDeTemperatura {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Temperatura {
+    private float valor;
+    private String tipoTemperatura; 
+
+
+
 }
