2760219b664341f097ce475a902315ce3fdd6229
Rafael José <braga3.coelho3@gmail.com>
2024-07-06 17:36:10 -0300

    Alterações para total funcionamento estatico.

--- ConversorDeTemperaturaEstatico/src/main/java/br/edu/ifnmg/conversordetemperaturaestatico/Temperatura.java
+++ ConversorDeTemperaturaEstatico/src/main/java/br/edu/ifnmg/conversordetemperaturaestatico/Temperatura.java
@@ -10,61 +10,58 @@ package br.edu.ifnmg.conversordetemperaturaestatico;
  */
 public class Temperatura {
     private static float valor;
-     
-    public void setValorTemperatura(float valor){
-        this.valor = valor;
-    }
     
-    public void exibeTemperatura(){
-        System.out.println("A temperatura é "+this.valor);
-    }
     //Converte o valor da temperatura de Fahrenheit para Celcius
-    public void fahrenheitParaCelsius()throws Exception{
-        if(this.valor<-459.67){
+    public static void fahrenheitParaCelsius()throws Exception{
+        if(Temperatura.valor<-459.67){
             throw new Exception("Valor inoperavel pois a temperatura está"
                     + " abaixo do zero absoluto");
         }
-        this.valor=(float)((this.valor-32)*5/9);
+        Temperatura.valor=(float)((Temperatura.valor-32)*5/9);
     }
-    public void fahrenheitParaKelvin()throws Exception{
-        if(this.valor<-459.67){
+    public static void fahrenheitParaKelvin()throws Exception{
+        if(Temperatura.valor<-459.67){
             throw new Exception("Valor inoperavel pois a temperatura está"
                     + " abaixo do zero absoluto");
         }
-        this.valor=(float)((this.valor-32)*5/9+273.15);
+        Temperatura.valor=(float)((Temperatura.valor-32)*5/9+273.15);
     }
     //Converte o valor da temperatura de Celcius para Kelvin
-    public void celciusParaKelvin()throws Exception{
-        if(this.valor<-273.15){
+    public static void celciusParaKelvin()throws Exception{
+        if(Temperatura.valor<-273.15){
             throw new Exception("Valor inoperavel pois a temperatura está"
                     + " abaixo do zero absoluto");
         }
-        this.valor=(float)(this.valor+273.15);
+        Temperatura.valor=(float)(Temperatura.valor+273.15);
     }
-    public void celciusParaFahrenheit()throws Exception{
-        if(this.valor<-273.15){
+    public static void celciusParaFahrenheit()throws Exception{
+        if(Temperatura.valor<-273.15){
             throw new Exception("Valor inoperavel pois a temperatura está"
                     + " abaixo do zero absoluto");
         }
-        this.valor=(float)((this.valor*9/5) + 32);
+        Temperatura.valor=(float)((Temperatura.valor*9/5) + 32);
     }
     //Converte o valor da temperatura de Kelvin para Fahrenheit
-    public void kelvinParaFahrenheit()throws Exception{
-        if(this.valor<0){
+    public static void kelvinParaFahrenheit()throws Exception{
+        if(Temperatura.valor<0){
             throw new Exception("Valor inoperavel pois a temperatura está"
                     + " abaixo do zero absoluto");
         }
-        this.valor=(float)(this.valor*9/5-459.67);
+        Temperatura.valor=(float)(Temperatura.valor*9/5-459.67);
     }
-    public void kelvinParaCelsius()throws Exception{
-        if(this.valor<0){
+    public static void kelvinParaCelsius()throws Exception{
+        if(Temperatura.valor<0){
             throw new Exception("Valor inoperavel pois a temperatura está"
                     + " abaixo do zero absoluto");
         }
-        this.valor=(float) ((this.valor)-273.15);
+        Temperatura.valor=(float) ((Temperatura.valor)-273.15);
     }
-    
-    
-    //Converte a temperatura para a temperatura designada no campo de argumento
+    public static float getValor() {
+        return valor;
+    }
+
+    public static void setValor(float valor) {
+        Temperatura.valor = valor;
+    }
 
 }
