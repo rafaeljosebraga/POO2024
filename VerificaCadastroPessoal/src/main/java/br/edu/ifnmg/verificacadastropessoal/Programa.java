fe9c73073ce87b3bbfbf92c63c59a95d13caf325
Rafael José <braga3.coelho3@gmail.com>
2024-07-08 15:36:19 -0300

    commit inicial da Atividade 3.

---
+++ VerificaCadastroPessoal/src/main/java/br/edu/ifnmg/verificacadastropessoal/Programa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifnmg.verificacadastropessoal;
+
+/**
+ *
+ * @author braga
+ */
+public class Programa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ VerificaCadastroPessoal/src/main/java/br/edu/ifnmg/verificacadastropessoal/VerificaCadastroPessoal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.verificacadastropessoal;
+
+/**
+ *
+ * @author braga
+ */
+public class VerificaCadastroPessoal {
+    public static int obterDigito(long cpf,int digito){
+
+    }
+    
+    public static int obterDigito(String cpf,int digito){
+        
+    }
+    
+    public static boolean isCpfValido(String cpf){
+        
+    }
+    
+}
