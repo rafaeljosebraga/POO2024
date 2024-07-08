8d1cb798b8505effcf0c4a7f47e6b4404581f1b4
Rafael José <braga3.coelho3@gmail.com>
2024-07-08 17:01:18 -0300

    Função obter digito feito,falta testes.

--- VerificaCadastroPessoal/src/main/java/br/edu/ifnmg/verificacadastropessoal/VerificaCadastroPessoal.java
+++ VerificaCadastroPessoal/src/main/java/br/edu/ifnmg/verificacadastropessoal/VerificaCadastroPessoal.java
@@ -10,7 +10,21 @@ package br.edu.ifnmg.verificacadastropessoal;
  */
 public class VerificaCadastroPessoal {
     public static int obterDigito(long cpf,int digito){
-
+        String aux;
+        aux = new String();
+        long cpfAux;
+        aux=Long.toString(cpf);
+        int tamanho= aux.length();
+        
+        //remove caracteres não numéricos
+        aux=aux.replaceAll("[^\\d]+", "");
+        
+        cpfAux=Long.parseLong(aux);
+        long auxLong;
+        auxLong=(long)Math.pow(10, digito);
+        cpfAux=cpfAux % auxLong;
+        cpfAux=(long) (cpfAux/Math.pow(10, digito-tamanho));
+        return (int)cpfAux;
     }
     
     public static int obterDigito(String cpf,int digito){
